a3a9edaa41eb3561e1cc726258fe95e35a1a0f21
ju young jeong <juyoungjeong@knuprime096ui-MacBook-Pro.local>
2021-05-31 00:54:34 +0900

    iteminfo view geometry
    iteminfo view geometry

--- WIT_combine/closetView.swift
+++ WIT_combine/closetView.swift
@@ -17,9 +17,14 @@ struct closetView: View {
         Product(name: "Half-neck Sweat L/beige", cate: "상의", subCate: "반팔", image : "product3", brand: "JEEP", price: 59000),
         Product(name: "아치 로고 맨투맨 네이비", cate: "상의", subCate: "반팔", image : "product4", brand: "COVERNOT", price: 57000),
         Product(name: "SIGNATURE HOODIE_lemon", cate: "상의", subCate: "반팔", image : "product5", brand: " 5252BYOIOI", price: 43371),
-        Product(name: "SIGNATURE HOODIE_light purple", cate: "상의", subCate: "반팔", image : "product6", brand: "5252BYOIOI", price: 43371),
-        Product(name: "Basic Logo Hood D/Pink", cate: "상의", subCate: "반팔", image : "product7", brand: "JEEP", price: 89000),
+        Product(name: "SIGNATURE HOODIE_light purple", cate: "상의", subCate: "후드", image : "product6", brand: "5252BYOIOI", price: 43371),
+        Product(name: "Basic Logo Hood D/Pink", cate: "상의", subCate: "후드", image : "product7", brand: "JEEP", price: 89000),
         Product(name: "purple 맨투맨", cate: "상의", subCate: "반팔", image : "product8", brand: "라퍼지스토어", price: 28000),
+        Product(name: "purple 맨투맨", cate: "상의", subCate: "긴팔", image : "product9", brand: "라퍼지스토어", price: 28000),
+        Product(name: "purple 맨투맨", cate: "상의", subCate: "긴팔", image : "product10", brand: "라퍼지스토어", price: 28000),
+        Product(name: "purple 맨투맨", cate: "상의", subCate: "긴팔", image : "product11", brand: "라퍼지스토어", price: 28000),
+        Product(name: "purple 맨투맨", cate: "상의", subCate: "긴팔", image : "product12", brand: "라퍼지스토어", price: 28000),
+        Product(name: "purple 맨투맨", cate: "상의", subCate: "반팔", image : "product13", brand: "라퍼지스토어", price: 28000),
         //Product(name: "my plastic bag_CQTAX21221PPX",image : "product9", brand: "CUSTOMELOW", price: 89000),
 //        Product(File: "upper10",Name: "빈티지라벨 맨투맨",Brand: "FILA",Price: 59000),
 //        Product(File: "upper11",Name: "부클 쿠퍼 로고 크루넥 브리티쉬그린",Brand: "COVERNAT ",Price: 57000),
@@ -46,6 +51,7 @@ struct closetView: View {
                                 Image(product.image)
                                     .resizable()
                                     .aspectRatio(contentMode: .fill)
+                                    .padding(3)
                             }.navigationBarHidden(true)
                         }
                     }
@@ -95,42 +101,46 @@ struct itemInfo : View {
     
     var body: some View {
         ScrollView{
-            VStack(alignment: .center) {
-                Image(product.image)
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 370, height: 370)
-                    .clipped()
-                
-                Text(product.name)
-                    .font(.custom("Roboto Regular", size: 20))
-                    .frame(width: 370, height: 60,alignment: .topLeading)
-                
-                
-                HStack{
-                    Text("\(product.cate) > \(product.subCate)").font(.custom("Roboto Regular", size: 16)).foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.37)))
+            GeometryReader { geometry in
+                VStack(alignment: .center) {
+                    Image(product.image)
+                        .resizable()
+                            .aspectRatio(contentMode: .fill)
+                        .frame(width: geometry.size.width*0.80, height: 350)
+                            .clipped()
+                        .frame(width: geometry.size.width*0.80, height: 350)
                     
-                    Spacer()
+                    Text(product.name)
+                        .font(.custom("Roboto Regular", size: 20))
+                        .frame(width: geometry.size.width*0.80, height: 60,alignment: .topLeading)
                     
-                    Text("\(product.price)₩")
-                        .font(.custom("Roboto Regular", size: 16))
+                    
+                    HStack{
+                        Text("\(product.cate) > \(product.subCate)").font(.custom("Roboto Regular", size: 16)).foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.37)))
                         
-                }.frame(width: 370, height: 60,alignment: .topLeading)
-                
-                Rectangle()
-                    .frame(width: 50, height: 50)
-                    .foregroundColor(product.avgColor)
-                    .cornerRadius(15)
-                    .onTapGesture {
-                        isClicked = true
-                    }
-                
-        //        Button("닫기") {
-        //            presentationMode.wrappedValue.dismiss()
-        //        }
-            }.navigationBarTitle(Text(product.brand))//, displayMode: .inline)
-            .sheet(isPresented: $isClicked) {
-                recommendItemView(product: product)
+                        Spacer(minLength: 50)
+                        
+                        Text("\(product.price)₩")
+                            .font(.custom("Roboto Regular", size: 16))
+                            
+                    }.frame(width: 370, height: 60,alignment: .topLeading)
+                    
+                    
+                    Rectangle()
+                        .frame(width: 50, height: 50)
+                        .foregroundColor(product.avgColor)
+                        .cornerRadius(15)
+                        .onTapGesture {
+                            isClicked = true
+                        }
+                    
+            //        Button("닫기") {
+            //            presentationMode.wrappedValue.dismiss()
+            //        }
+                }.navigationBarTitle(Text(product.brand))//, displayMode: .inline)
+                .sheet(isPresented: $isClicked) {
+                    recommendItemView(product: product)
+                }
             }
         }
     }
